790df9071592883b7a4edf23c70fea8083f830f6
Yan Victor Da Silva Lima <yan.lima@scseduca.com.br>
2025-04-16 21:51:35 -0300

    finalizacao tela de login no jsx

--- senai-gpt-web-react/src/pages/login/index.jsx
+++ senai-gpt-web-react/src/pages/login/index.jsx
@@ -1,6 +1,42 @@
 import "./login.css";
+import logo from "../../assets/imgs/Chat.png";
+import { useState } from "react";
+
+
 
 function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // function onLoginClick {
+
+  //   Gera um novo escopo.
+
+  // }
+
+  const onLoginClick = async () => {
+
+      let response = await fetch ("https://senai-gpt-api.azurewebsites.net/login", { 
+
+
+        headers: {
+          "Content-Type": "application/json"
+
+        },
+        method: "POST", //Metedo que envia dados
+        body: JSON.stringify({
+          email: email, 
+          password: password
+        })
+
+
+      });
+
+        console.log(response)
+  
+    //Mantem o escopo existente.
+  }
 
 
   //JavaScript
@@ -19,18 +55,18 @@ function Login() {
         <div className="login-container">
 
 
-          <img className="logo" src="../.vscode/assets/imgs/Chat.png" alt="Logo do SenaiGPT" />
+          <img className="logo" src={logo} alt="Logo do SenaiGPT" />
 
           <h1
             id="meutitulo"
             class="titulo"> Login </h1>
 
-          <input className="inpt" type="email"
+          <input className="inpt" value={email} onChange={event => setEmail(event.target.value)} type="email"
             placeholder="Insira o e-mail" />
-          <input className="inpt" type="password"
+          <input className="inpt" value={password} onChange={event => setPassword(event.target.value)} type="password"
             placeholder="Insira a senha" />
 
-          <button className ="botao-entrar">Entrar</button>
+          <button className ="botao-entrar" onClick={() => onLoginClick()}>Entrar</button> 
 
         </div>
 
