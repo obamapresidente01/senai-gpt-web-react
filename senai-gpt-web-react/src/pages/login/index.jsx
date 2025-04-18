3afd213b0a1f0dff6ace71b02d7b799c8abf8b54
Yan Victor Da Silva Lima <yan.lima@scseduca.com.br>
2025-04-17 21:18:12 -0300

    Finalizacao da tela de Login

--- senai-gpt-web-react/src/pages/login/index.jsx
+++ senai-gpt-web-react/src/pages/login/index.jsx
@@ -17,24 +17,63 @@ function Login() {
 
   const onLoginClick = async () => {
 
-      let response = await fetch ("https://senai-gpt-api.azurewebsites.net/login", { 
+    let response = await fetch("https://senai-gpt-api.azurewebsites.net/login", {
 
 
-        headers: {
-          "Content-Type": "application/json"
+      headers: {
+        "Content-Type": "application/json"
 
-        },
-        method: "POST", //Metedo que envia dados
-        body: JSON.stringify({
-          email: email, 
-          password: password
-        })
+      },
+      method: "POST", //Metedo que envia dados
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
 
 
-      });
+    })
 
-        console.log(response)
-  
+    if (response.ok == true) {
+
+      alert("Login Realizado com sucesso");
+
+      console.log(response);
+
+      let json = await response.json();
+
+      let token = json.accessToken;
+
+      console.log("Token" + token);
+
+      //LOCALSTORAGE
+
+      localStorage.setItem("meuToken", token);
+
+      //COOKIES
+      // function setCookie(name, value, days) {
+      //   const date = new Date();
+      //   date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000)); // dias > ms
+      //   const expires = "expires=" + date.toUTCString();
+      //   document.cookie = `${name} =${value}; ${expires} path=/` ;
+      // }
+
+      // setCookie("meuToken", token, 7)
+
+      window.location.href = "/chat";
+
+    } else {
+
+      if (response.status == 401) {
+
+        alert("Credenciais Incorretas. Tente Novamente.");
+
+      } else {
+
+        alert("Erro inesperado aconteceu, caso persista contate os administradores");
+
+      }
+    }
+
     //Mantem o escopo existente.
   }
 
@@ -59,14 +98,14 @@ function Login() {
 
           <h1
             id="meutitulo"
-            class="titulo"> Login </h1>
+            className="titulo"> Login </h1>
 
           <input className="inpt" value={email} onChange={event => setEmail(event.target.value)} type="email"
             placeholder="Insira o e-mail" />
           <input className="inpt" value={password} onChange={event => setPassword(event.target.value)} type="password"
             placeholder="Insira a senha" />
 
-          <button className ="botao-entrar" onClick={() => onLoginClick()}>Entrar</button> 
+          <button className="botao-entrar" onClick={() => onLoginClick()}>Entrar</button>
 
         </div>
 
